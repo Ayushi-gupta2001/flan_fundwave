cf5373ec6dd98b0a4ad5ced7ee959c2eca1c2c47
Ayushi-gupta2001 <g.dibs2001@gmail.com>
2023-01-17 08:01:29 +0000

    updated files

--- shared/fundwave.sh
+++ shared/fundwave.sh
@@ -1,19 +1,19 @@
 #!/bin/bash 
 
-file1="/home/fundwave/flan/shared/combined.txt"
-file2="/home/fundwave/flan/shared/ips.txt"
+file1="/home/fundwave/shared/combined.txt"
+file2="/home/fundwave/shared/ips.txt"
 a=""
 while IFS= read -r line
 do 
    echo "$line"
    echo "$line" > $file2
-   cd "/home/fundwave/flan/"
+   cd "/home/fundwave/"
    make html 
-   cd "/home/fundwave/flan/shared/reports"
+   cd "/home/fundwave/shared/reports"
    for dir in *; do  
 	a="$dir"; 
    done
-   mv "$a" "${line}.html"
-   #aws s3 sync $b s3://mywavebucket
+   sudo mv "$a" "${line}.html"
+   aws s3 sync reports s3://mywavebucket
 done < $file1 
 
---
+++ shared/sendtos3.sh
@@ -0,0 +1,6 @@
+#AWS_ACCESS_KEY_ID="AKIA4QSS3DIK5KJAW5EZ"
+#AWS_SECRET_ACCESS_KEY="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"
+#S3_BUCKET="mywavebucket"
+
+cd /home/fundwave/shared/
+aws s3 sync abc  s3://mywavebucket
